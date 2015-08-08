84553b375d3790ea37f7b8b83ac346299382d847
Danilo <danilo_kun@hotmail.com>
2015-08-09 08:24:46 +0900

    user register interface concertada

--- app/app.js
+++ app/app.js
@@ -14,67 +14,8 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
-  
-   Template.register.events({
-        'submit form': function(event) {
-            event.preventDefault();
-            console.log("Form submitted.");
-        }
-    });
-    
-    Template.register.events({
-    'submit form': function(event){
-        event.preventDefault();
-        var emailVar = event.target.registerEmail.value;
-        var passwordVar = event.target.registerPassword.value;
-        console.log("Form submitted.");
-        }
-    });
-
-    Template.login.events({
-        'submit form': function(event) {
-            event.preventDefault();
-            var emailVar = event.target.loginEmail.value;
-            var passwordVar = event.target.loginPassword.value;
-            console.log("Form submitted.");
-        }
-    });
-    
-    Template.register.events({
-        'submit form': function(event) {
-            event.preventDefault();
-            var emailVar = event.target.registerEmail.value;
-            var passwordVar = event.target.registerPassword.value;
-            Accounts.createUser({
-                email: emailVar,
-                password: passwordVar
-            });
-        }
-    });
-    
-    Template.login.events({
-        'submit form': function(event){
-            event.preventDefault();
-            var emailVar = event.target.loginEmail.value;
-            var passwordVar = event.target.loginPassword.value;
-            Meteor.loginWithPassword(emailVar, passwordVar);
-        }
-    });
-    
-    Template.dashboard.events({
-        'click .logout': function(event){
-            event.preventDefault();
-        }
-    });
-    
-    Template.dashboard.events({
-        'click .logout': function(event){
-            event.preventDefault();
-            Meteor.logout();
-        }
-    });
 }
-
+  
 if (Meteor.isServer) {
 	Meteor.startup(function () {
 	  UploadServer.init({
