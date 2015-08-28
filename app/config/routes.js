a1939c9305767ae71734b72e6e358e8b69e8c5f2
Bruno Vieira Costa <brunopsycon@gmail.com>
2015-08-28 20:27:40 -0300

    começando a diferenciar as estruturas

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,25 +1,24 @@
 Router.configure({
 	layoutTemplate: 'application_layout'
 });
-
 Router.route('/',function(){
 	this.render('homepage');
 });
 Router.route('/panel',function(){
 	this.render('user_panel');
 });
-Router.route('/new_comic',function(){
-	this.render('new_comic');
+Router.route('/edit_comic',function(){
+	this.render('edit_comic');
 });
-
+Router.route('/edit_page',function(){
+	this.render('edit_page');
+});
 Router.route('/signin',function(){
 	this.render('new_user');
 });
-
 Router.route('/login',function(){
 	this.render('login');
 });
-
 Router.route('/display',function(){
 	this.render('display');
 });
