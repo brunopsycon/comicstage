d052a1468348de64dbd616c90ea77c32046e7e40
daniloggoncalves <danilo_kun@hotmail.com>
2015-08-30 04:10:39 +0900

    segunda tentativa

--- app/both/collections/init.js
+++
@@ -1,4 +0,0 @@
-Users=Meteor.users
-Comics=new Mongo.Collection("comics");
-Chapter=new Mongo.Collection("chapters");
-Page=new Mongo.Collection("pages");
--- app/both/router/routes.js
+++ app/both/router/routes.js
@@ -1,23 +1,12 @@
 Router.route('/', {
   name: 'home'
 });
-Router.route('/panel', {
-  name: 'panel',
-  controller: 'PanelController'
-});
-Router.plugin('ensureSignedIn', {
-  only: ['panel']
-});
-Router.route('/edit_comic',function(){
-	this.render('edit_comic');
-});
-Router.route('/edit_chapter',function(){
-	this.render('edit_chapter');
-});
-Router.route('/edit_page',function(){
-	this.render('edit_page');
-});
-Router.route('/display',function(){
-	this.render('display');
+
+Router.route('/dashboard', {
+  name: 'dashboard',
+  controller: 'DashboardController'
 });
 
+Router.plugin('ensureSignedIn', {
+  only: ['dashboard']
+});
