ff55e5819addd4de53bb1bf2d4f5e54fd4788bbe
Bruno Vieira Costa <brunopsycon@gmail.com>
2015-09-03 16:12:08 -0300

    criação de helpers de forms em comics

---
+++ app/structure/comics/edit_helpers.js
@@ -0,0 +1,14 @@
+if (Meteor.isClient){
+	Template.edit_comic.helpers({
+		form_data :function(){
+			return [
+				{form_name: 'name',label_text: 'name',form_type:'text'},
+				{form_name: 'genre',label_text: 'genre',form_type:'text'},
+				{form_name: 'visible',label_text: 'visible',form_type:'checkbox'},
+				{form_name: 'description',label_text: 'description',form_type:'text'},
+				{form_name: 'icon_image',label_text: 'icon image',form_type:'text'},
+				{form_name: 'landscape_image',label_text: 'landscape image',form_type:'text'}
+			];
+		}
+	});
+}
